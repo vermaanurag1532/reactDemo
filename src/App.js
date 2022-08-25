c55357251f9fa58c49d444721585cdcd70df6bd6
vermaanurag1532 <104077846+vermaanurag1532@users.noreply.github.com>
2022-08-25 23:29:05 +0530

    Revert "fix: form error fix and formatting"

--- src/App.js
+++ src/App.js
@@ -1,47 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import NewExpense from './component/NewExpense/NewExpense';
-import ExpenseItem from './component/ExpenseItem/ExpenseItem';
+import NewExpense from "./component/newexpense";
+import ExpenseItem from "./component/expenseitem";
 
-const Dummy_expenses = [
-  {
-    id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-  {
-    id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
+const Dummy_expenses = [{
+        id: "e1",
+        title: "Toilet Paper",
+        amount: 94.12,
+        date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+        id: "e3",
+        title: "Car Insurance",
+        amount: 294.67,
+        date: new Date(2021, 2, 28),
+    },
+    {
+        id: "e4",
+        title: "New Desk (Wooden)",
+        amount: 450,
+        date: new Date(2021, 5, 12),
+    },
 ];
 
 function App() {
-  const [expense, setExpenses] = useState(Dummy_expenses);
+    const [expense, setExpenses] = useState(Dummy_expenses);
 
-  const addExpenseHandler = (expense) => {
-    setExpenses((preExpenses) => {
-      return [...preExpenses, expense];
-    });
-  };
+    const addExpenseHandler = (expense) => {
+        setExpenses((preExpenses) => {
+            return [...preExpenses, expense];
+        });
+    };
 
-  return (
-    <div>
-      <NewExpense onAddExpense={addExpenseHandler} />{' '}
-      {expense.map((expenses, index) => (
-        <ExpenseItem title={expenses.title} amount={expenses.amount} date={expenses.date} key={index} />
-      ))}{' '}
-    </div>
-  );
+    return ( <
+            div >
+            <
+            NewExpense onAddExpense = { addExpenseHandler }
+            />{" "} {
+            expense.map((expenses, index) => ( <
+                ExpenseItem title = { expenses.title }
+                amount = { expenses.amount }
+                date = { expenses.date }
+                key = { index }
+                />
+            ))
+        } { " " } <
+        /div>
+);
 }
 
 export default App;
---
+++ src/component/newexpense.js
@@ -0,0 +1,22 @@
+import "./newexpense.css";
+import ExpenseForm from "./expenseform";
+
+const NewExpense = (props) => {
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString(),
+        };
+        props.onAddExpense(expenseData);
+    };
+
+    return ( <
+        div className = "new-expense" >
+        <
+        ExpenseForm onSaveExpenseData = { saveExpenseDataHandler }
+        />{" "} <
+        /div>
+    );
+};
+
+export default NewExpense;
