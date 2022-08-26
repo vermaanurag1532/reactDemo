46336e6b24fe5455e8bec788fac520e744c3811b
vermaanurag1532 <vermaanurag1532@gmail.com>
2022-08-26 18:41:21 +0530

    finish

--- src/App.js
+++ src/App.js
@@ -1,54 +1,54 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import NewExpense from "./component/NewExpense/NewExpense";
-import ExpenseItem from "./component/ExpenseItem/ExpenseItem";
-import ExpenseChart from "./component/ExpenseChart/ExpenseChart";
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
 
-const Dummy_expenses = [{
-        id: "e1",
-        title: "Toilet Paper",
+const DUMMY_EXPENSES = [{
+        id: 'e1',
+        title: 'Toilet Paper',
         amount: 94.12,
         date: new Date(2020, 7, 14),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
-        id: "e3",
-        title: "Car Insurance",
+        id: 'e3',
+        title: 'Car Insurance',
         amount: 294.67,
         date: new Date(2021, 2, 28),
     },
     {
-        id: "e4",
-        title: "New Desk (Wooden)",
+        id: 'e4',
+        title: 'New Desk (Wooden)',
         amount: 450,
         date: new Date(2021, 5, 12),
     },
 ];
 
-function App() {
-    const [expense, setExpenses] = useState(Dummy_expenses);
+const App = () => {
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
     const addExpenseHandler = (expense) => {
-        setExpenses((preExpenses) => {
-            return [...preExpenses, expense];
+        setExpenses((prevExpenses) => {
+            return [expense, ...prevExpenses];
         });
     };
+
+    // return React.createElement(
+    //   'div',
+    //   {},
+    //   React.createElement('h2', {}, "Let's get started!"),
+    //   React.createElement(Expenses, { items: expenses })
+    // );
 
     return ( <
         div >
         <
-        ExpenseChart expenses = {}
-        /> <NewExpense onAddExpense={addExpenseHandler} / > { " " } {
-            expense.map((expenses, index) => ( <
-                ExpenseItem title = { expenses.title }
-                amount = { expenses.amount }
-                date = { expenses.date }
-                key = { expenses.id }
-                />
-            ))
-        } { " " } <
+        NewExpense onAddExpense = { addExpenseHandler }
+        /> <
+        Expenses items = { expenses }
+        /> <
         /div>
     );
-}
+};
 
 export default App;
--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,15 +1,14 @@
-import React from "react";
+import React from 'react';
 
-import ChartBar from "./ChartBar";
-import "./Chart.css";
+import ChartBar from './ChartBar';
+import './Chart.css';
 
 const Chart = (props) => {
-    const dataPointValues = props.dataPoints.map((dataPoint => dataPoint.value));
-
+    const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
     const totalMaximum = Math.max(...dataPointValues);
 
     return ( <
-        div className = "chart" > { " " } {
+        div className = 'chart' > {
             props.dataPoints.map((dataPoint) => ( <
                 ChartBar key = { dataPoint.label }
                 value = { dataPoint.value }
@@ -17,7 +16,7 @@ const Chart = (props) => {
                 label = { dataPoint.label }
                 />
             ))
-        } { " " } <
+        } <
         /div>
     );
 };
